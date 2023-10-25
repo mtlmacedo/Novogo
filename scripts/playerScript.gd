90ab72277bcf057a261665a6472a6b9fa811b074
mtlmacedo <42045383+mtlmacedo@users.noreply.github.com>
2023-10-25 12:39:33 -0300

    movement improvements and flip_h

--- scripts/playerScript.gd
+++ scripts/playerScript.gd
@@ -1,30 +1,40 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 350.0
+const JUMP_VELOCITY = -550.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var motion = Vector2.ZERO
-
+func sprite_handler(direction):
+	
+	if direction > 0:
+		print("direita", direction)
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.play("run")
+	elif direction < 0:
+		print("esquerda", direction)
+		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.play("run")
+	else :
+		$AnimatedSprite2D.play("idle")
+		
 func _process(delta):
 	# Add the gravity.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction == 0 :
+		velocity.x = direction * SPEED
+	elif direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	sprite_handler(direction)
+	
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		$AnimatedSprite2D.play("jump")
-	else :
-		var direction = Input.get_axis("ui_left", "ui_right")
-		if direction == 0 :
-			velocity.x = direction * SPEED
-			$AnimatedSprite2D.play("idle")
-		elif direction:
-			velocity.x = direction * SPEED
-			$AnimatedSprite2D.play("run")
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-			$AnimatedSprite2D.play("run")
-
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY		
